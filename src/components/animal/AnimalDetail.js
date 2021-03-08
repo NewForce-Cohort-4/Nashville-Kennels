c9313186f3db18476dd01493b9f457bda02988af
Tommy Spurlock <tspurloc@gmail.com>
2021-03-08 15:10:24 -0500

    Completed edit functionality

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -6,12 +6,9 @@ import { useParams, useHistory } from "react-router-dom";
 export const AnimalDetail = () => {
   const { getAnimalById, releaseAnimal } = useContext(AnimalContext);
 
-
   const [animal, setAnimal] = useState({});
   const { animalId } = useParams();
   const history = useHistory();
-
-
 
   useEffect(() => {
     console.log("useEffect", animalId);
@@ -21,13 +18,11 @@ export const AnimalDetail = () => {
     });
   }, []);
 
-
   const handleRelease = () => {
-    releaseAnimal(animal.id)
-      .then(() => {
-        history.push("/animals")
-      })
-  }
+    releaseAnimal(animal.id).then(() => {
+      history.push("/animals");
+    });
+  };
 
   return (
     <section className="animal">
@@ -36,8 +31,14 @@ export const AnimalDetail = () => {
       {/* What's up with the question mark???? See below.*/}
       <div className="animal__location">Location: {animal.location?.name}</div>
       <div className="animal__owner">Customer: {animal.customer?.name}</div>
+      <button
+        onClick={() => {
+          history.push(`/animals/edit/${animal.id}`);
+        }}
+      >
+        Edit
+      </button>
       <button onClick={handleRelease}>Release Animal</button>
-
     </section>
   );
 };
--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -41,6 +41,17 @@ export const AnimalProvider = (props) => {
         .then(getAnimals)
 }
 
+  const updateAnimal = animal => {
+  return fetch(`http://localhost:8088/animals/${animal.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(animal)
+  })
+    .then(getAnimals)
+}
+
   /*
         You return a context provider which has the
         `animals` state, `getAnimals` function,
@@ -54,7 +65,8 @@ export const AnimalProvider = (props) => {
         getAnimals,
         addAnimal,
         getAnimalById,
-        releaseAnimal
+        releaseAnimal,
+        updateAnimal
       }}
     >
       {props.children}
